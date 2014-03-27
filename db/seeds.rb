0964927e8628996f2baa4d23a26958e6a4de569f
Jessiah Ratliff <jessiahr@gmail.com>
2014-03-27 01:07:04 -0700

    responsive tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating things...."
+25.times.each do |i|
+  Thing.create(:name => "Thing #{i}")
+end
